fe23f6a0c2afeafeb91cb50e81755bd6a8e40be0
50160567 <corbin.mckee.ryan@gmail.com>
2021-01-28 17:10:04 -0700

    Revert "m"
    This reverts commit 9bf57b04feaa1e23bdd68bed240c4dd750b3b152.

---
+++ core/files.py
@@ -0,0 +1,8 @@
+from yaml import load as yload, FullLoader
+class Data:
+    def __init__(self, filename:str):
+        self.file = filename
+
+    def yaml_read(self):
+        with open(f"data/{self.file}.yml", "r") as f:
+            return yload(f, Loader=FullLoader)
