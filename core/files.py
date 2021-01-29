9bf57b04feaa1e23bdd68bed240c4dd750b3b152
50160567 <corbin.mckee.ryan@gmail.com>
2021-01-28 17:08:35 -0700

    m

--- core/files.py
+++
@@ -1,8 +0,0 @@
-from yaml import load as yload, FullLoader
-class Data:
-    def __init__(self, filename:str):
-        self.file = filename
-
-    def yaml_read(self):
-        with open(f"data/{self.file}.yml", "r") as f:
-            return yload(f, Loader=FullLoader)
